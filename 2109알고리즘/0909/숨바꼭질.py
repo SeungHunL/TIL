e66653d76e693c07a68202e387c6e4eafaf4831d
SeungHunL <bgd02013@gmail.com>
2021-09-09 23:39:49 +0900

    +숨바꼭질

--- 2109알고리즘/0909/숨바꼭질.py
+++ 2109알고리즘/0909/숨바꼭질.py
@@ -5,17 +5,20 @@ N, K = map(int, sys.stdin.readline().split())
 que = deque()
 que.append((0, N))
 used = [0] * 100001
+# 최대 길이 만큼 할당
 used[N] = 1
+
 while que:
     t, x = que.popleft()
     if x == K:
         print(t)
         break
     t += 1
+    # used 중복제거 및 횟수 카운팅
     if x <= 50000 and used[2 * x] == 0:
         used[2 * x] = 1
         que.append((t, 2 * x))
-    if used[x + 1] == 0:
+    if x < 100000 and used[x + 1] == 0:
         used[x + 1] = 1
         que.append((t, x + 1))
     if x > 0 and used[x - 1] == 0:
