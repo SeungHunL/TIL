a680766ce78ec7d7de24e1ac8d3d6c778aa2159c
SeungHunL <bgd02013@gmail.com>
2021-09-19 23:53:28 +0900

    이진 검색 트리

--- 2109알고리즘/today/이진 검색 트리.py
+++ 2109알고리즘/today/이진 검색 트리.py
@@ -3,41 +3,37 @@ import sys
 input = sys.stdin.readline
 
 
-class Node:
-    def __init__(self, data, left, right):
+class Node():
+    def __init__(self, data, left=None, right=None):
         self.data = data
         self.left = left
         self.right = right
 
 
-def postorder(treedic,key):
-    if not treedic[key][0]:
-        postorder(treedic, treedic[key][0])
-    if not treedic[key][1]:
-        postorder(treedic, treedic[key][1])
-    print(key)
+def postorder(node):
+    if node.left != None:
+        postorder(tree[node].left)
+    if node.right != None:
+        postorder(tree[node].right)
+    print(node.data, end='')
 
+
+start = int(input())
 
 tree = {}
-N = []
-start = int(input())
-N.append(start)
-tree[start] = [0, 0]
-while N:
-    N.append(int(input()))
-    if N[-1] > N[-2]:
-        i = 2
-        while 1:
-            
-            if N[-i - 1] > N[-1] > N[-i]:
-                tree[N[-i]][1] = N[-1]
-                tree[N[-1]]=[0,0]
-                break
-            else:
-                i+=1
-    else:
-        tree[N[-2]][0] = N[-1]
-        tree[N[-1]] = [0,0]
-    print(tree)
 
-postorder(tree,start)
+nlist = [start]
+while True:
+    try:
+        nlist.append(int(input()))
+        if nlist[-1] < nlist[-2]:
+            tree[nlist[-2]] = Node(nlist[-2], nlist[-1])
+            tree[nlist[-1]] = Node(None)
+        else:
+            if not tree[start].right and nlist[-1] > start:
+                tree[start].right = nlist[-1]
+            if nlist[-2] < nlist[-1] < nlist[-3]:
+                tree[nlist[-2]].right = nlist[-1]
+    except:
+        break
+postorder(tree[start])
