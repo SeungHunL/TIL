c8df756209546e3a3c355a15bd23cad86742d3b5
SeungHunL <bgd02013@gmail.com>
2021-10-10 23:46:57 +0900

    1010

--- 2110/today/로봇 청소기.py
+++ 2110/today/로봇 청소기.py
@@ -1,19 +1,30 @@
 import sys
 input=sys.stdin.readline
+
 def robot(y,x,heading):
-    maps[y][x]=1
-    dy=[0,1,0,-1]
-    dx=[-1,0,1,0]
-    ty = y + dy[heading]
-    tx = x + dx[heading]
-    c=4
-    while c:
-        while not maps[ty][tx]:
-            maps[ty][tx]=2
-            ty += dy[heading]
-            tx += dx[heading]
-        heading=(heading-1)%4
-        c-=1
+    # 1 현재 위치를 청소
+    c=[0]*4
+    while y!=0 or x!=0 or y!=N-1 or x!=M-1 :
+        maps[y][x]=1
+
+        #2 현재 위치(y,x)에서 heading을 기준으로 왼쪽
+        ly = y + dy[(heading - 1) % 4]
+        lx = x + dx[(heading - 1) % 4]
+        while 1:
+            if not maps[ly][lx]:
+                heading -= 1  # 왼쪽 방향에 청소 안 한 공간 존재, 회전
+                y, x = ly, lx  # 한 칸 전진
+                break
+            else:
+                c[heading]=1
+                heading -= 1
+            if c=[1,1,1,1]:
+                y-=dy[heading]
+                x-=dx[heading]
+
+dy=[1,0,-1,0]
+dx=[0,1,0,-1]
+
 N,M=map(int,input().split())
 r,c,d=map(int,input().split())
 maps=[]
